7eca1262599cf3e09fa5ad617e352abbeee23da0
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-03-08 19:56:59 -0800

    user route;

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,28 @@ router.get('/users', (req, res) => {
     res.send('User List');
 });
 
-router.get('/users/new', (req, res) => {
+router.get('/new', (req, res) => {
     res.send("User New Form");
 });
+
+router.post('/', (req, res) => {
+    res.send("Create User");
+})
+
+router.route('/:id').get((req, res) => {
+    console.log(req.user);
+    res.send(`Get User With ID ${req.params.id}`); 
+}).put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`);
+});
+
+
+const users = [{ name: "peep"}, { name: "athena"}]
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next();
+})
 
 module.exports = router;
