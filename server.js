22900a33194cc06a8a1083f488c92c0fd3afb626
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-03-15 18:48:50 -0700

    authenticate;

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const userRouter = require('./routes/users');
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("views"));
+app.set('view engine', 'ejs');
 
 app.get('/', logger, (req, res) => {
     console.log("Online");
-    res.render("index", { text: "world"});
+    res.render("index");
 });
-
-app.use('/users', userRouter);
 
 function logger(req, res, next) {
     console.log(req.originalUrl);
     next();
 }
 
-app.listen(3000);
+function isAuth(req, res, next) {
+    const password = req.body.password;
+    if (user.password === password) {
+        next();
+    } else {
+        res.status(401);
+        res.send('Access forbidden');
+    }
+}
+
+app.listen(3000); 
+
